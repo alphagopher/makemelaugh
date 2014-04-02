95b535abd5f0905b480c071348f0011cbd4e531e
Andrew Porter <porter.andrewsteven@gmail.com>
2014-04-01 23:22:01 -0500

    Performs HTTP GET and displays JSON from ICNDb  in the UI thread which is bad. This would normally throw a NetworkOnMainThreadException but I added a ThreadPolicy to permit this temporarily. Another requirement here was to add android.permission.INTERNET in Manifest.
    TO DO: Implement AsyncTask to make this a non-blocker action off the
    main UI thread.
    TO DO: Format JSON output
    TO DO: Read back text to user (if possible)

--- MakeMeLaugh/src/com/aporter/makemelaugh/MainActivity.java
+++ MakeMeLaugh/src/com/aporter/makemelaugh/MainActivity.java
@@ -1,64 +1,117 @@
 package com.aporter.makemelaugh;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.aporter.makemelaugh.R;
+import com.google.android.glass.app.Card;
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.os.StrictMode;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+	
+	private static final String TAG = "MakeMeLaugh";
+	private static final String URL = "http://api.icndb.com/jokes/random";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+		
+		try {
+			
+			Card card = new Card(this);
+			card.setText(connect(URL));
+		    card.setFootnote(R.string.text_footer);
+			setContentView(card.toView());
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
+		} catch (Exception e) {
+			
+			Log.e(TAG, e.getMessage());
+			System.exit(1);
 		}
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+	}
+	
+	public static String connect(String urlValue) {
+
+		String result = "An error occured.";
+		
+	    HttpClient httpclient = new DefaultHttpClient();
+
+	    // Prepare a request object
+	    HttpGet httpget = new HttpGet(urlValue); 
+	    
+	    // Execute the request
+	    HttpResponse response;
+	    
+	    try {
+	        response = httpclient.execute(httpget);
+	        
+	        // Examine the response status
+	        Log.i(TAG,response.getStatusLine().toString());
+
+	        // Get hold of the response entity
+	        HttpEntity entity = response.getEntity();
+	        
+	        // If the response does not enclose an entity, there is no need
+	        // to worry about connection release
+
+	        if (entity != null) {
+
+	            // A Simple JSON Response Read
+	            InputStream instream = entity.getContent();
+	            
+	            result =  convertStreamToString(instream);
+	            
+	            // now you have the string representation of the HTML request
+	            instream.close();	            
+	        }
+
+	    } catch (Exception e) { 
+	    	System.out.println("9");
+	    	e.printStackTrace();
+	    	}
+		return result;
 	}
 
+	    private static String convertStreamToString(InputStream is) {
+	    /*
+	     * To convert the InputStream to String we use the BufferedReader.readLine()
+	     * method. We iterate until the BufferedReader return null which means
+	     * there's no more data to read. Each line will appended to a StringBuilder
+	     * and returned as String.
+	     */
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+	    StringBuilder sb = new StringBuilder();
+
+	    String line = null;
+	    try {
+	        while ((line = reader.readLine()) != null) {
+	            sb.append(line + "\n");
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	        	is.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return sb.toString();
+	}
 }
