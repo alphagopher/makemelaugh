6d14df7a1b45293ff855c997413ff55f6e069185
Andrew Porter <porter.andrewsteven@gmail.com>
2014-04-14 22:00:10 -0500

    Moved API call to AsyncTask, updated main icon, added icon for "Read out loud".

--- MakeMeLaugh/gen/com/aporter/makemelaugh/R.java
+++ MakeMeLaugh/gen/com/aporter/makemelaugh/R.java
@@ -22,7 +22,8 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int chuck_norris=0x7f020000;
+        public static final int ic_read_aloud_50=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f090001;
@@ -39,7 +40,9 @@ public final class R {
         public static final int action_settings=0x7f060003;
         public static final int app_name=0x7f060000;
         public static final int start_makemelaugh=0x7f060001;
+        public static final int text_default_joke=0x7f060004;
         public static final int text_footer=0x7f060002;
+        public static final int text_url=0x7f060005;
     }
     public static final class style {
         /** 
--- MakeMeLaugh/src/com/aporter/makemelaugh/MainActivity.java
+++ MakeMeLaugh/src/com/aporter/makemelaugh/MainActivity.java
@@ -1,125 +1,78 @@
 package com.aporter.makemelaugh;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
 import com.aporter.makemelaugh.R;
 import com.google.android.glass.app.Card;
-
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
 
 public class MainActivity extends Activity {
-	
-	private static final String TAG = "MakeMeLaugh";
+
+	//private static final String TAG = "MakeMeLaugh";
 	private static final String URL = "http://api.icndb.com/jokes/random";
+	private Card card;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-		StrictMode.setThreadPolicy(policy);
-		
-		String jokeText = "Sad face. We couldn't find a joke :(";
-		
+
 		try {
-			
-			Card card = new Card(this);
-			
-			String rawJSON = getRandomJoke(URL);
-			JSONObject valueObject = new JSONObject(rawJSON).getJSONObject("value");
-			jokeText = valueObject.getString("joke");
-			
-			card.setText(jokeText);
-		    card.setFootnote(R.string.text_footer);
+
+			card = new Card(this);
+			card.setText(R.string.text_default_joke);
+			card.setFootnote(R.string.text_footer);
 			setContentView(card.toView());
+			
+			GetJokeTask task = new GetJokeTask();
+			task.execute(new String[] { URL });
 
 		} catch (Exception e) {
-			
-			Log.e(TAG, e.getMessage());
+
+			e.printStackTrace();
 			System.exit(1);
 		}
 
 	}
-	
-	public static String getRandomJoke(String urlValue) {
 
-		String result = "An error occured.";
-		
-	    HttpClient httpClient = new DefaultHttpClient();
+	private class GetJokeTask extends AsyncTask<String, Void, String> {
 
-	    // Prepare a request object
-	    HttpGet httpGet = new HttpGet(urlValue); 
-	    
-	    // Execute the request
-	    HttpResponse response;
-	    
-	    try {
-	        response = httpClient.execute(httpGet);
-	        
-	        // Examine the response status
-	        Log.i(TAG,response.getStatusLine().toString());
+		@Override
+		protected String doInBackground(String... urls) {
+			String response = "";
+			for (String url : urls) {
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(url);
+				try {
+					HttpResponse execute = client.execute(httpGet);
+					InputStream content = execute.getEntity().getContent();
 
-	        // Get hold of the response entity
-	        HttpEntity entity = response.getEntity();
-	        
-	        // If the response does not enclose an entity, there is no need
-	        // to worry about connection release
+					BufferedReader buffer = new BufferedReader(
+							new InputStreamReader(content));
+					String s = "";
+					while ((s = buffer.readLine()) != null) {
+						response += s;
+					}
+					response = new JSONObject(response).getJSONObject("value").getString("joke");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return response;
+		}
 
-	        if (entity != null) {
-
-	            // A Simple JSON Response Read
-	            InputStream inputStream = entity.getContent();
-	            
-	            result =  convertStreamToString(inputStream);
-	            
-	            // now you have the string representation of the HTML request
-	            inputStream.close();	            
-	        }
-
-	    } catch (Exception e) { 
-	    	System.out.println("9");
-	    	e.printStackTrace();
-	    	}
-		return result;
-	}
-
-	private static String convertStreamToString(InputStream inputStream) {
-	    /*
-	     * To convert the InputStream to String we use the BufferedReader.readLine()
-	     * method. We iterate until the BufferedReader return null which means
-	     * there's no more data to read. Each line will appended to a StringBuilder
-	     * and returned as String.
-	     */
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-	    StringBuilder stringBuilder = new StringBuilder();
-
-	    String line = null;
-	    try {
-	        while ((line = reader.readLine()) != null) {
-	        	stringBuilder.append(line + "\n");
-	        }
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    } finally {
-	        try {
-	        	inputStream.close();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	    return stringBuilder.toString();
+		// @Override
+		protected void onPostExecute(String result) {
+			card.setText(result);
+			setContentView(card.toView());
+		}
 	}
 }
